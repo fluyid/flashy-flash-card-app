09ec0b4de64704df55a34a195140a44e655ad1e8
Kailash Nath <kailashnathashok@gmail.com>
2024-04-27 06:41:28 +0300

    created a function that would show the next card when the right or wrong button is pressed

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from tkinter import Tk, Label, Button, PhotoImage, Canvas
+from tkinter import Tk, Button, PhotoImage, Canvas
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 language_name_font = ("Ariel", 40, "italic")
 translated_font = ("Ariel", 60, "bold")
+
+data = pandas.read_csv("data/Top 100 German to English Words - Sheet1.csv")
+to_learn = data.to_dict(orient="records")
+
+# Functions
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    print(current_card["German"])
+    canvas.itemconfig(card_title, text="German")
+    canvas.itemconfig(card_word, text=current_card["German"])
+
 
 # Window
 window = Tk()
@@ -18,15 +33,17 @@ card_back = PhotoImage(file="images/card_back.png")
 # Canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front)
-name_of_language = canvas.create_text(400, 150, text="German", font=language_name_font, fill="black")
-translated_version = canvas.create_text(400, 263, text="ich", font=translated_font, fill="black")
+card_title = name_of_language = canvas.create_text(400, 150, text="", font=language_name_font, fill="black")
+card_word = translated_version = canvas.create_text(400, 263, text="", font=translated_font, fill="black")
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Button
-right_button = Button(image=right_image, highlightthickness=0, borderwidth=0)
+right_button = Button(image=right_image, highlightthickness=0, borderwidth=0, command=next_card)
 right_button.grid(row=1, column=1)
 
-wrong_button = Button(image=wrong_image, highlightthickness=0, borderwidth=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, borderwidth=0, command=next_card)
 wrong_button.grid(row=1, column=0)
+
+next_card()
 
 window.mainloop()
